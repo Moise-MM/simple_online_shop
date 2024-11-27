f2887f2c50d7fa0bf8d3b6174167bf8d93fcb162
moses-mm <moisemutangilay47@gmail.com>
2024-11-27 12:15:57 +0100

    Create component app-layout compnent

---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="{{ asset('front_end_assets/assets/favicon.ico') }}" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ asset('front_end_assets/css/styles.css') }}" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+       @include('layouts.partials._navbar')
+
+
+        <main>
+            {{ $slot }}
+        </main>
+
+        
+        <!-- Footer-->
+        @include('layouts.partials._footer')
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{ asset('front_end_assets/js/scripts.js') }}"></script>
+    </body>
+</html>
