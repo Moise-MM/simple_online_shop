5a1896776edbbfa8f9728130aa6c3fd2624bc8b8
moses-mm <moisemutangilay47@gmail.com>
2024-11-27 13:57:50 +0100

    Install PHP_CodeSniffer in the project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,11 +12,13 @@ class HomeController extends Controller
         $viewData = [];
         $viewData["title"] = "Home Page - Online Store";
         $viewData['hero_title'] = "Everything You Need, All in One Place";
-        $viewData['hero_description'] = "Whether you're upgrading your home or shopping for gifts, find everything you need right here.";
-        return view('home.index',[
+        $viewData['hero_description'] = "Whether you're upgrading your home or shopping for gifts,
+                                         find everything you need right here.";
+        return view('home.index', [
             'viewData' => $viewData
         ]);
     }
+
 
 
     public function about():View
@@ -27,7 +29,7 @@ class HomeController extends Controller
         $viewData['description'] = "This is an about page ...";
         $viewData ['author'] = "Developed by: Moise-MM";
 
-        return view('home.about',[
+        return view('home.about', [
             'viewData' => $viewData
         ]);
     }
